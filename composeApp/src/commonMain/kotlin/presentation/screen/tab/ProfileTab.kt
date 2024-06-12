83ce58f67eaf7f699760a34953525b7a40a9724c
Jesús Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2024-06-11 18:26:10 -0700

    Profile with saved-completed challenges and simple graph

--- composeApp/src/commonMain/kotlin/presentation/screen/tab/ProfileTab.kt
+++ composeApp/src/commonMain/kotlin/presentation/screen/tab/ProfileTab.kt
@@ -21,8 +21,10 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.FractionalThreshold
@@ -46,11 +48,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import bubble.composeapp.generated.resources.Res
@@ -101,6 +108,131 @@ object ProfileTab : Tab {
                 modifier = Modifier
                     .fillMaxWidth()
             ) {
+                item {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .padding(8.dp)
+                                .size(64.dp)
+                                .clip(CircleShape)
+                                .background(Color.Gray)
+                        ) {
+
+                        }
+                        Column {
+                            Text(
+                                "Nombre del usuario",
+                                style = MaterialTheme.typography.subtitle1,
+                            )
+                            Text(
+                                "Correo electrónico",
+                                style = MaterialTheme.typography.subtitle2,
+                            )
+                        }
+                    }
+                }
+                item {
+                    Text(
+                        text = "Tiempo en pantalla",
+                        style = MaterialTheme.typography.caption,
+                        modifier = Modifier
+                            .padding(horizontal = 16.dp, vertical = 4.dp)
+                    )
+                }
+                item {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp, vertical = 4.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(16.dp)
+                        ) {
+                            Text(
+                                text = "Hoy, 10 de junio",
+                                style = MaterialTheme.typography.subtitle2,
+                            )
+                            Text(
+                                text = "1 h 39 min",
+                                style = MaterialTheme.typography.h4,
+                            )
+                            val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
+                            val textMeasurer = rememberTextMeasurer()
+                            val textStyle = MaterialTheme.typography.caption
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(128.dp)
+                                    .drawBehind {
+                                        drawRect(
+                                            color = Color.White,
+                                            topLeft = Offset(0f, 0f),
+                                            size = size,
+                                        )
+                                        repeat(7) { index ->
+                                            if (index != 0) {
+                                                drawLine(
+                                                    color = Color.LightGray,
+                                                    start = Offset(0f, (size.height / 7) * index),
+                                                    end = Offset(
+                                                        size.width - (size.width / 8),
+                                                        (size.height / 7) * index
+                                                    ),
+                                                    strokeWidth = 2f
+                                                )
+                                            }
+                                        }
+                                        drawText(
+                                            textMeasurer = textMeasurer,
+                                            text = "10",
+                                            topLeft = Offset(
+                                                size.width - (size.width / 8) + 2.dp.toPx(),
+                                                (size.height / 7) - 6.dp.toPx()
+                                            ),
+                                            style = textStyle
+                                        )
+                                        drawText(
+                                            textMeasurer = textMeasurer,
+                                            text = "0",
+                                            topLeft = Offset(
+                                                size.width - (size.width / 8) + 2.dp.toPx(),
+                                                size.height - (size.height / 7) - 6.dp.toPx()
+                                            ),
+                                            style = textStyle.copy(
+                                                color = Color.LightGray
+                                            ),
+                                        )
+                                        listOf("D", "L", "M", "M", "J", "V", "S", "").forEachIndexed { index, dayOfTheWeekFirstLetter ->
+                                            drawLine(
+                                                color = Color.LightGray,
+                                                start = Offset((size.width / 8) * index, size.height / 7),
+                                                end = Offset((size.width / 8) * index, size.height),
+                                                strokeWidth = 2f,
+                                                pathEffect = pathEffect
+                                            )
+                                            if (dayOfTheWeekFirstLetter != "") {
+                                                drawText(
+                                                    textMeasurer = textMeasurer,
+                                                    text = dayOfTheWeekFirstLetter,
+                                                    topLeft = Offset(
+                                                        ((size.width / 8) * index) + 2.dp.toPx(),
+                                                        size.height - 16.dp.toPx()
+                                                    ),
+                                                    style = textStyle.copy(
+                                                        color = if (index == 2) Color.Black
+                                                        else Color.LightGray,
+                                                    )
+                                                )
+                                            }
+                                        }
+                                    }
+                            ) {
+                            }
+                        }
+                    }
+                }
                 item {
                     Text(
                         "Desafíos guardados",
